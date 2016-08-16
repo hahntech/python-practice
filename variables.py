24ae27b47120799f33cb37575a6ccb700afa1ea3
hahntech <dryheat3@hahn-tech.com>
2016-08-15 18:47:31 -0700

    Added variables file

---
+++ variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+my_integer = 1000
+my_decimal = 10.9
+my_string = "Hello There!"
+
+print(my_integer)
+print(type(my_integer))
+print(my_decimal)
+print(type(my_decimal))
+print(my_string)
+print(type(my_string))
