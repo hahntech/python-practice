7b0cb24afba9202f1c25f77f427fbfe3ef0aad87
hahntech <dryheat3@hahn-tech.com>
2016-08-16 18:51:11 -0700

    added dictionary_def file

---
+++ dictionary_def.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# this is a JSON object
+my_dictionary = {
+    "a key": "a value",
+    "another key with numeric value": 3.6,
+    True: "A boolean as a key",
+    5: "An Int as a key"
+}
+print(str(my_dictionary))
