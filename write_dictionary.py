d7b65c88b350fcf7c5a8de4166d2d005b197c527
hahntech <dryheat3@hahn-tech.com>
2016-08-17 18:17:45 -0700

    Updaed write_directory file

--- write_dictionary.py
+++ write_dictionary.py
@@ -11,6 +11,26 @@ print(str(my_dog))
 # second argument to the get method is the default value
 # returned in case the key does not exist
 my_dog["age"] = my_dog.get("age", 0) + 1
+print(str("After get() + 1: " +str(my_dog)))
+
+# an alternative way of handling a potential missing key
+if "age" in my_dog:
+    my_dog["age"] += 1
+else:
+    my_dog["age"] = 1
+
+# if condition:
+#     action
+# elif another condition:
+#     another action
+# elif another condition:
+#     another action
+# ...
+# else:
+#     action if none of the above conditions is met
+
+print(str("After if/else: " + str(my_dog)))
+
 my_dog["girlfriend_name"] = "Quiara"
 
 print(str(my_dog))
@@ -18,5 +38,3 @@ print(str(my_dog))
 del my_dog["girlfriend_name"]
 
 print(str(my_dog))
-
-print("This section begins a key check demo.")
