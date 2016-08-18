18e9ae1681c470fa083ab94606e4724abf418484
hahntech <dryheat3@hahn-tech.com>
2016-08-17 18:57:05 -0700

    added string_ops file

---
+++ string_ops.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+my_string = "Hello World!!"
+
+print(my_string)
+print(len(my_string))
+print("He" in my_string)
+print(my_string[3])
+print(my_string[1:4])
+print(my_string[1:4] + my_string[6:8])
