f7f30d3c30dd3fb7e1ec2621c4f7c938865dc917
hahntech <dryheat3@hahn-tech.com>
2016-08-17 19:02:33 -0700

    added while-else file

---
+++ while-else.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+i = 0
+my_list = [1, 2, 3, 4, 5, 6]
+
+while i < len(my_list):
+    print(str("element #" + str(my_list[i])))
+    i += 1
+else:
+    print("end of elments")
+
+print("Begin section about 'break' and 'continue'.")
