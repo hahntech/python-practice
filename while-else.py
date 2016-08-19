f8f9cdfaee4ccaf3e0cb0632f1fdefd6c42cdf8b
hahntech <dryheat3@hahn-tech.com>
2016-08-18 18:49:02 -0700

    updated while-else file

--- while-else.py
+++ while-else.py
@@ -9,4 +9,20 @@ while i < len(my_list):
 else:
     print("end of elments")
 
-print("Begin section about 'break' and 'continue'.")
+print("Begin section about 'break' and 'continue'")
+
+j = 0
+myOtherList = [1, 2, 3, 4, 5, 6]
+
+while j < len(myOtherList):
+    if myOtherList[j] == 5:
+        j += 1
+        break
+        # '% 2 == 0' checks for even numbers
+    if myOtherList[j] % 2 == 0:
+        j += 1
+        continue
+    print(myOtherList[j])
+    j += 1
+else:
+    print("no element is left to print")
